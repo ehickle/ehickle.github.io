b87d79b3f2a371ecded4b7b9724f2906072135c8
Evan Hickle <evanhickle@Evans-MacBook-Pro.local>
2017-05-23 19:50:24 -0500

    Add javascript event which makes picture visible when a specific option is chosen in a dropdown menu

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("The script is running!");
+
+function makeImageVisible() {
+	var photo = document.getElementById("grotto");
+	var name = dropdown1.value
+	
+	if (name == "Wyatt") {
+	photo.style.visibility = "visible";
+	}
+}
+
+var dropdown1 = document.getElementById("pick1");
+ dropdown1.addEventListener("change", makeImageVisible);
